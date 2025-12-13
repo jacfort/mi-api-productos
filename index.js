6a77dc0ea784c0cccee8d1977ba6e5c80ea3447d
jacfort <fortuseba@gmail.com>
2025-12-13 19:13:38 -0300

    u54

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-import dotenv from 'dotenv';
-dotenv.config(); 
-
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
 import productsRoutes from './src/routes/products.routes.js';
 import authRoutes from './src/routes/auth.routes.js';
-
 import { notFoundHandler, errorHandler } from './src/middlewares/error.middleware.js';
 import './src/config/firebase.js';
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); // ← ESTO
 
 app.use('/api/products', productsRoutes);
 app.use('/auth', authRoutes);
