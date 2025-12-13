ca864cc1d9762f33faa6a0270dbd91ef7add4a0d
jacfort <fortuseba@gmail.com>
2025-12-13 18:18:13 -0300

    set

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,26 +1,44 @@
 import jwt from 'jsonwebtoken';
 
 export const login = (req, res) => {
-  const { email, password } = req.body;
+  console.log('ENV USER:', process.env.ADMIN_USER);
+  console.log('ENV PASS:', process.env.ADMIN_PASS);
+  console.log('BODY:', req.body);
 
-  // Usuario de prueba (hardcodeado)
-  if (email !== 'admin@admin.com' || password !== '1234') {
+  const { user, password } = req.body;
+
+  if (
+    user !== process.env.ADMIN_USER ||
+    password !== process.env.ADMIN_PASS
+  ) {
     return res.status(401).json({ message: 'Credenciales inválidas' });
   }
 
-  const payload = {
-    email,
-    role: 'admin'
-  };
-
-  const token = jwt.sign(
-    payload,
-    process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES }
-  );
-
-  res.json({
-    token,
-    type: 'Bearer'
-  });
+  res.json({ ok: true });
 };
+// 
+
+// export const login = (req, res) => {
+//   const { email, password } = req.body;
+
+//   // Usuario de prueba (hardcodeado)
+//   if (email !== 'admin@admin.com' || password !== '1234') {
+//     return res.status(401).json({ message: 'Credenciales inválidas' });
+//   }
+
+//   const payload = {
+//     email,
+//     role: 'admin'
+//   };
+
+//   const token = jwt.sign(
+//     payload,
+//     process.env.JWT_SECRET,
+//     { expiresIn: process.env.JWT_EXPIRES }
+//   );
+
+//   res.json({
+//     token,
+//     type: 'Bearer'
+//   });
+// };
